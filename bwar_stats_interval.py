7bf499d054c29f064ef378adbf93f0d1cd914b2f
theundyingphoenix <42872423+theundyingphoenix@users.noreply.github.com>
2019-06-18 14:41:17 -0700

    Update bwar_stats_interval.py

--- bwar_stats_interval.py
+++ bwar_stats_interval.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import pandas as pd 
 import datetime
@@ -14,8 +15,13 @@ def bwar_bat_interval(day):
 	file = "war_archive-"+day
 	zip_file = file+".zip"
 	url = "http://www.baseball-reference.com/data/"+zip_file
-	print("Retrieving data from 2013-03-29 to "+zip_file+"...\n")
+	print("Retrieving data from "+zip_file+"...\n")
+	
 	s = requests.get(url)
+
+	if s.status_code == 404 or s.status_code != 200:
+		print(s.status_code)
+		return None
 
 	with open(zip_file, 'wb') as code:
 		code.write(s.content)
@@ -23,29 +29,57 @@ def bwar_bat_interval(day):
 	with ZipFile(zip_file) as myzip:
 	    with myzip.open('war_daily_bat.txt') as myfile:
 	    	c = pd.read_csv(io.StringIO(myfile.read().decode('utf-8')))
+	    	
 
-
-	cols_to_keep = ['name_common', 'mlb_ID', 'player_ID', 'year_ID', 'team_ID', 'stint_ID', 'lg_ID', 
+	cols_to_keep = ['name_common', 'player_ID', 'year_ID', 'team_ID', 'stint_ID', 'lg_ID', 
 					'pitcher','G', 'PA', 'salary', 'runs_above_avg', 'runs_above_avg_off','runs_above_avg_def',
 					'WAR_rep','WAA','WAR']
+	
 
-	stopping_year = 2013
-	c = c[c.year_ID >= 2013]
-	c = c[c.lg_ID != "AL"]
-	c = c[c.pitcher == "N"]	
-	# c = c[c.name_common == "Lane Adams"]			
+	
 	return c[cols_to_keep]				
 
 client = MongoClient('localhost', 27017)
-db = client['baseball_stats']
-collection = 'war_stats_2017_05_08'
-db_cm = db[collection]
+print("Acquired mongodb client...\n")
 
-interval = "2017-05-08"
-print("Finding data for "+interval)
-lf = bwar_bat_interval(interval)
-print("DataFrame collected")
+# import the dates to have the war_daily_bat.txt
+under_interval = []
+interval = []
+print("Opening seasons.txt & season.txt...\n")
+f = open("/home/user/seasons.txt", 'r')
+ff = open("/home/user/season.txt", 'r')
 
-json_data = json.loads(lf.to_json(orient='records'))
-db_cm.remove()
-db_cm.insert(json_data)	
+# remove the \n
+l = f.readline()[:-1]
+ll = ff.readline()[:-1]
+while l:
+	interval.append(l)
+	under_interval.append(ll)
+	l = f.readline()[:-1]
+	ll = ff.readline()[:-1]
+f.close()
+ff.close()	
+
+print("Collected the dates of stats to import...\n")
+
+for i in range(len(interval)):
+	db = client['baseball_stats_'+interval[i][:4]]
+	collection = 'war_archive_'+under_interval[i]
+	db_cm = db[collection]
+	
+	print("Finding data for "+interval[i])
+	lf = bwar_bat_interval(interval[i])	
+
+	lf = lf[lf.year_ID >= 2013]
+	lf = lf[lf.lg_ID != "AL"]
+
+	if lf.empty:
+		continue		
+	
+	print("DataFrame collected")
+	
+	json_data = json.loads(lf.to_json(orient='records'))
+	db_cm.remove()
+	db_cm.insert(json_data)	
+	print("JSON data added to MongoDB...")
+	os.remove("war_archive-"+interval[i]+".zip")
