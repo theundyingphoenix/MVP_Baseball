e045d09e9f406d86c6c646a1540f0bec3d89d4f7
theundyingphoenix <42872423+theundyingphoenix@users.noreply.github.com>
2019-06-19 06:05:59 -0700

    Update bwar_stats_interval.py

--- bwar_stats_interval.py
+++ bwar_stats_interval.py
@@ -5,8 +5,49 @@ import datetime
 import io
 from bs4 import BeautifulSoup
 from zipfile import ZipFile
-from pymongo import MongoClient
 import json
+import pymongo
+
+def catalogue_database(client):
+	client = pymongo.MongoClient('localhost', 27017)
+	list_of_databases = client.database_names()
+
+
+	db_prefix = 'baseball_stats_'
+	collection_prefix = 'war_archive_'
+
+	dates = []
+	underscored_dates = []
+	f = open("/home/user/seasons.txt", 'r')
+	line = f.readline()
+	while line:
+		dates.append(line)
+		line = f.readline()
+
+	years_present = {}
+	for u in range(len(dates)):
+		year = dates[u][:4]	
+		db = db_prefix+year
+	
+		for i in range(len(list_of_databases)):
+			if year in list_of_databases[i]:
+				years_present[year] = True
+
+	# for the year's db we want to mark the values in underscored_dates
+	# as True to catalogue the archives present, then take the latest
+	# date as the place to start
+
+	for k in years_present:
+		# access the db
+		db = db_prefix+k
+		db = pymongo.database.Database(client,db)
+		archived_dates = db.list_collection_names()
+		for a in archived_dates:
+			underscored_dates.append(a)
+		
+	return sorted(underscored_dates)		
+
+
 
 def bwar_bat_interval(day):
 	
@@ -19,9 +60,10 @@ def bwar_bat_interval(day):
 	
 	s = requests.get(url)
 
-	if s.status_code == 404 or s.status_code != 200:
+	if s.status_code == 200:
 		print(s.status_code)
-		return None
+	else:
+		return None	
 
 	with open(zip_file, 'wb') as code:
 		code.write(s.content)
@@ -39,34 +81,39 @@ def bwar_bat_interval(day):
 	
 	return c[cols_to_keep]				
 
-client = MongoClient('localhost', 27017)
+client = pymongo.MongoClient('localhost', 27017)
 print("Acquired mongodb client...\n")
 
 # import the dates to have the war_daily_bat.txt
-under_interval = []
-interval = []
+# for storing archives in correct format
+under_interval = [] 
+# for the list of dates to traverse in url format
+interval = [] 
 print("Opening seasons.txt & season.txt...\n")
 f = open("/home/user/seasons.txt", 'r')
 ff = open("/home/user/season.txt", 'r')
 
+
 # remove the \n
 l = f.readline()[:-1]
-ll = ff.readline()[:-1]
 while l:
 	interval.append(l)
-	under_interval.append(ll)
+	under_interval.append(l.replace("-","_"))
 	l = f.readline()[:-1]
-	ll = ff.readline()[:-1]
 f.close()
-ff.close()	
 
 print("Collected the dates of stats to import...\n")
 
 for i in range(len(interval)):
+	# to prevent process from starting from the beginning
+	present_archives = catalogue_database(client)
+	most_recently_added = present_archives[-1]
+
 	db = client['baseball_stats_'+interval[i][:4]]
 	collection = 'war_archive_'+under_interval[i]
 	db_cm = db[collection]
-	
+
+
 	print("Finding data for "+interval[i])
 	lf = bwar_bat_interval(interval[i])	
 
@@ -82,4 +129,7 @@ for i in range(len(interval)):
 	db_cm.remove()
 	db_cm.insert(json_data)	
 	print("JSON data added to MongoDB...")
+	saved_checkpoint = open("/home/user/checkpoint.txt", 'w')
+	saved_checkpoint.write(interval[i])
+	saved_checkpoint.close()
 	os.remove("war_archive-"+interval[i]+".zip")
